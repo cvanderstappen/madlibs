932f3d4b99ba2a8e43169d587aef23241efe6f0d
Chet vanderStappen <thechetvanderstappen@gmail.comgit config --global user.name Chet>
2018-06-14 08:47:18 -0600

    built functional components and added inputs

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Bottega Madlibs</h1>
+      <p>Fill out the forms below and click generate
+      to see the mad libs story</p>
+    </div>
+  )
+}
+
+export default Header;
