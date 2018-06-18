1059d3a1abfc6f5b0c639034776e33f9c6693530
Chet vanderStappen <thechetvanderstappen@gmail.comgit config --global user.name Chet>
2018-06-18 09:43:47 -0600

    styled input grid

--- src/components/header.js
+++ src/components/header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div className="header">
       <div className="header__skew"></div>
-      <div className="header__border"></div>
+        <div className="header__border"></div>
       <h1>Bottega Madlibs</h1>
       <p>Fill out the forms below and click generate
       to see the mad libs story</p>
